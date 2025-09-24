edaca6cadf579509119ee5fb2b0e83091d4bfbf6
ADAMOKEEFFE-STUDENT <g00434897@atu.ie>
2025-09-24 14:42:01 +0100

    Setting up springboot project

---
+++ src/main/java/ie/atu/week2lab/Week2LabApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.week2lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week2LabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Week2LabApplication.class, args);
+	}
+
+}
